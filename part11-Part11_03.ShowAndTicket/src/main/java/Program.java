6bd00ddf5368f657a772be6224a28ca94b47ca83
VY <veasna.youphon@live.com>
2025-05-08 13:53:06 -0700

    complete part11-3

---
+++ part11-Part11_03.ShowAndTicket/src/main/java/Program.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // you can test your classes here
+        Show show = new Show();
+        Ticket ticket = new Ticket();
+
+    }
+}
