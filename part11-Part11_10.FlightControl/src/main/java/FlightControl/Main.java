76668be8c24b498feaf8e3bfeb638594b188bb0d
VY <veasna.youphon@live.com>
2025-05-09 13:56:29 -0700

    update part11

---
+++ part11-Part11_10.FlightControl/src/main/java/FlightControl/Main.java
@@ -0,0 +1,12 @@
+package FlightControl;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Write the main program here. It is useful to create some classes of your own.
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
